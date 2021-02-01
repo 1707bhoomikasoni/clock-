64b14b63302f68c71fe41b7edb69e25ba333905e
1707bhoomikasoni <74401608+1707bhoomikasoni@users.noreply.github.com>
2021-02-01 14:05:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var hr 
-var mn 
-var sc 
+ var mn 
+ var sc 
 
 var scAngle;
 var mnAngle;
@@ -8,56 +8,53 @@ var hrAngle;
 
 function setup() {
   createCanvas(800,400);
-  hr = hour()
-  mn = minute()
-  sc = second()
-}
+ }
 
 function draw() {
-  background("black");  
+  background("black"); 
+   hr = hour()
+   mn = minute()
+   sc = second()
+ 
+  translate(200,200)
+  rotate(-90)
+
   scAngle = map(sc, 0, 60, 0, 360)
   mnAngle = map(mn, 0, 60, 0, 360)
-  hrAngle = map(hr, 0, 12, 0, 360)
-
+  hrAngle = map(hr%12, 0, 12, 0, 360)
  
   push()
   rotate(scAngle)
-  translate(0,0)
   stroke("red")
   strokeWeight(7)
-  line(400,200,480,170)
+  line(0,0,100,0)
   pop()
 
   push()
   rotate(mnAngle)
-  translate(0,0)
   stroke("yellow")
   strokeWeight(7)
-  line(400,200,450,160)
+  line(0,0,75,0)
   pop()
 
   push()
   rotate(hrAngle)
-  translate(0,0)
   stroke("blue")
   strokeWeight(7)
-  line(400,200,440,200)
+  line(0,0,50,0)
   pop()
 
+noFill()
+
   stroke("red")
- strokeWeight("7")
- fill("black")
-  arc(405,200,100,150,0,scAngle)
+ strokeWeight(7)
+  arc(0,0,300,300,0,scAngle)
 
   stroke("yellow")
- strokeWeight("7")
- fill("black")
-  arc(394,200,110,150,0,mnAngle)
+ strokeWeight(7)
+  arc(0,0,280,280,0,mnAngle)
 
   stroke("blue")
- strokeWeight("7")
- fill("black")
-  arc(392,190,100,150,0,hrAngle)
-
-
+ strokeWeight(7)
+  arc(0,0,260,260,0,hrAngle)
 }
